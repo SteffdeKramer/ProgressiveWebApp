7e95eef509672d6f8516de5f4404229325f263cb
Stef <stefdekramer3@gmail.com>
2019-03-20 10:19:19 +0100

    code formatting

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
-navigator.serviceWorker.register('sw.js')
-.then(reg => console.log('SW registered!', reg))
-.catch(err => console.log('Boo!', err));
+navigator.serviceWorker
+  .register("sw.js")
+  .then(reg => console.log("SW registered!", reg))
+  .catch(err => console.log("Boo!", err));
 
 fetch("https://cmgt.hr.nl:8000/api/projects/")
-  .then((resp) => resp.json()) // Transform the data into json
+  .then(resp => resp.json()) // Transform the data into json
   .then(function(data) {
-      console.log(data);
+    console.log(data);
 
-      for (let i = 0; i < data.projects.length; i++) {
-          const element = data.projects[i];
+    for (let i = 0; i < data.projects.length; i++) {
+      const element = data.projects[i];
 
-          let author = data.projects[i].author;
-          let description = data.projects[i].description;
-          let img = "https://cmgt.hr.nl:8000/" + data.projects[i].headerImage;
+      let author = data.projects[i].author;
+      let description = data.projects[i].description;
+      let img = "https://cmgt.hr.nl:8000/" + data.projects[i].headerImage;
 
-          AppendObject(author, description, img);
-          
-      }
+      AppendObject(author, description, img);
+    }
+  });
 
-    });
+(function() {
+  "use strict";
 
+  //check for support
+  if (!("indexedDB" in window)) {
+    console.log("This browser doesn't support IndexedDB");
+    return;
+  }
+
+  var dbPromise = idb.open("test-db1", 1);
+})();
 
 function AppendObject(ProjectTitle, ProjectDescription, ProjectImage) {
-   let div = document.createElement("div");
-   let title = document.createElement("h2");
-   let description = document.createElement("p");
-   let img = document.createElement("img");
+  let div = document.createElement("div");
+  let title = document.createElement("h2");
+  let description = document.createElement("p");
+  let img = document.createElement("img");
 
-   title.innerHTML = ProjectTitle;
-   description.innerHTML = ProjectDescription;
-   img.src = ProjectImage;
+  title.innerHTML = ProjectTitle;
+  description.innerHTML = ProjectDescription;
+  img.src = ProjectImage;
 
-   title.className = "projectTitle";
-   description.className = "projectDescription";
-   img.className = "projectImage";
+  title.className = "projectTitle";
+  description.className = "projectDescription";
+  img.className = "projectImage";
 
-   div.appendChild(title);
-   div.appendChild(img);
-   div.appendChild(description);
+  div.appendChild(title);
+  div.appendChild(img);
+  div.appendChild(description);
 
-
-   document.getElementById("projects").appendChild(div);
+  document.getElementById("projects").appendChild(div);
 }
--- sw.js
+++ sw.js
@@ -1,18 +1,56 @@
-self.addEventListener('install', event => {
-    console.log('V1 installing…');
-  
-    // Files to cach
-    event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(
-              [
-                '/css/bootstrap.css',
-                '/css/main.css',
-                '/js/bootstrap.min.js',
-                '/js/jquery.min.js',
-                '/offline.html'
-              ]
-            );
-        })
-    );
+var cacheName = 'PWA';
+
+var filesToCache = [
+  'app.js',
+  'main.css',
+  'index.html',
+];
+
+// Install
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
+
+// Activation
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Network only
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
+
+// Cache falling back to the network
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Network falling back to the cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// i am a new service worker. 
